502b166e4ab8014dc4d66d67d4cbe773407307f2
jpedograu <144135652+jpedograu@users.noreply.github.com>
2024-03-01 16:57:43 -0300

    opan

---
+++ conexao.php
@@ -0,0 +1,16 @@
+<?php
+$host ='localhost';
+$user ='root';
+$senha='';
+$bd = 'sistema';
+
+$conexao = new mysqli($host,$user,$senha,$bd);
+ if (!$conexao){
+    echo'erro ao se comunicar como bd'.mysqli_connect_errno();
+ }
+ else{
+    echo 'deu certo a conexao'
+ }
+ 
+
+?>
