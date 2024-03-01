8ef73dbd7c9f0c84ed1bd1ffe1f6983682100e8a
jpedograu <144135652+jpedograu@users.noreply.github.com>
2024-03-01 11:10:26 -0300

    opan

---
+++ cadrasto1.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/cad.css">
+    <title>Formulario</title>
+</head>
+<body>
+       <div class="box">
+        <form action="">
+            <fieldset>
+                <legend><b>Formulário de clientes</b></legend>
+                <br>
+                <div class="inputbox">,
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome"class="labelInput">nome completo</label>
+                </div>
+                </br>
+
+                <br>
+                <div class="inputbox">,
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email"class="labelInput">email</label>
+                </div>
+                </br>
+                <br>
+                <div class="inputbox">,
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
+                    <label for="telefone"class="labelInput">telefone</label>
+                </div>
+                </br>
+                
+                <div class="inputbox">,
+                    <label for="data_nascimento"><b>Data de nascimento</b></label>
+                    <input type="date" name="data_nascimento" id="data_nascimento" required>
+                  <br></br>
+
+                </div>
+                <br><br>
+                <div class="inputbox">,
+                    <input type="text" name="cidade" id="cidade" class="inputUser" required>
+                    <label for="cidade"class="labelInput">cidade</label>
+                    </div>
+                </br>
+                
+
+                <br><br>
+                <div class="inputbox">,
+                    <input type="text" name="estado" id="estado" class="inputUser" required>
+                    <label for="estado"class="labelInput">estado</label>
+                    </div>
+                </br>
+                
+
+                <br>
+                <div class="inputbox">,
+                    <input type="text" name="endereço" id="endereço" class="inputUser" required>
+                    <label for="endereço"class="labelInput">endereço</label>
+                
+                </div>
+                </br><br>
+
+                <div class="inputbox">,
+                    <input type="passord" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha"class="labelInput">senha</label>
+              
+                </div>
+                </br><br>
+                                
+              <button>envia</button>
+            
+
+                                
+                
+            
+                
+
+                
+            
+
+                </fieldset>
+
+
+
+
+
+
+
+
+
+                </div>
+</body>
+</html
